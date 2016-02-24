1a8fe4d694770a1be0cf819f8ab4c492843c7b6a
Benjamin Charity <ben@benjamincharity.com>
2016-02-24 07:14:22 -0500

    added events service to module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { FlickityConfigProvider } from './flickity.provider'
 import { FlickityService } from './flickity.service';
+import { EventsService } from './events.service';
 import { FlickityDirective } from './flickity.directive';
 import { FlickityNextDirective } from './next/flickityNext.directive';
 import { FlickityPreviousDirective } from './previous/flickityPrevious.directive';
@@ -7,6 +8,7 @@ import { FlickityPreviousDirective } from './previous/flickityPrevious.directive
 angular.module('bc.Flickity', [])
     .provider('FlickityConfig', FlickityConfigProvider)
     .service('FlickityService', FlickityService)
+    .service('EventsService', EventsService)
     .directive('bcFlickity', FlickityDirective)
     .directive('bcFlickityNext', FlickityNextDirective)
     .directive('bcFlickityPrevious', FlickityPreviousDirective)
