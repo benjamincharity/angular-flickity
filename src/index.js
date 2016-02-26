dad15fdee085bbf57783d6eb485965b266884015
Benjamin Charity <ben@benjamincharity.com>
2016-02-26 05:53:03 -0500

    removed events service

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { FlickityConfigProvider } from './flickity.provider'
 import { FlickityService } from './flickity.service';
-import { EventsService } from './events.service';
 import { FlickityDirective } from './flickity.directive';
 import { FlickityNextDirective } from './next/flickityNext.directive';
 import { FlickityPreviousDirective } from './previous/flickityPrevious.directive';
@@ -8,7 +7,6 @@ import { FlickityPreviousDirective } from './previous/flickityPrevious.directive
 angular.module('bc.Flickity', [])
     .provider('FlickityConfig', FlickityConfigProvider)
     .service('FlickityService', FlickityService)
-    .service('EventsService', EventsService)
     .directive('bcFlickity', FlickityDirective)
     .directive('bcFlickityNext', FlickityNextDirective)
     .directive('bcFlickityPrevious', FlickityPreviousDirective)
