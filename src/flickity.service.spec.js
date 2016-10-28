86166aaa834551e47a0c24c3ae7b145ec1635b93
Benjamin Charity <ben@benjamincharity.com>
2016-10-28 14:31:18 -0400

    create test

--- src/flickity.service.spec.js
+++ src/flickity.service.spec.js
@@ -1,4 +1,4 @@
-/* global set */
+/* global set, loadFixtures */
 describe('FlickityService', () => {
     let $compile;
     let $rootScope;
@@ -18,6 +18,12 @@ describe('FlickityService', () => {
             //this.$scope.done = jasmine.createSpy('done');
             //this.$scope.fail = jasmine.createSpy('fail');
             //this.$scope.progress = jasmine.createSpy('progress');
+            this.$scope.slides = [
+                'http://cdn.benjamincharity.com/codepen/angular-flickity/slide1.jpg',
+                'http://cdn.benjamincharity.com/codepen/angular-flickity/slide2.jpg',
+                'http://cdn.benjamincharity.com/codepen/angular-flickity/slide3.jpg',
+                'http://cdn.benjamincharity.com/codepen/angular-flickity/slide4.jpg',
+            ];
         });
 
     });
@@ -26,7 +32,6 @@ describe('FlickityService', () => {
     beforeEach(function() {
         this.compileDirective = function(template) {
             this.element = this.$compile(template)(this.$scope);
-            this.vm = this.element.isolateScope().vm;
         };
     });
 
@@ -39,32 +44,29 @@ describe('FlickityService', () => {
 
         it(`should instantiate a Flickity instance`, function(done) {
             const template = angular.element(`
-                <div
-                    class="demo__slides"
-                    bc-flickity="{{ flickityOptions }}"
-                    bc-flickity-id="demoId1"
-                    id="demo-slider"
-                >
-                    <figure class="demo__slide" data-ng-repeat="slide in slides track by $index">
-                        <img data-ng-src="{{ ::slide }}" alt="" />
+                <div id="js_demo">
+                    <figure data-ng-repeat="slide in slides track by $index">
+                        <img data-ng-src="{{ slide }}" alt="" />
                     </figure>
-
                 </div>
             `);
-            jasmine.set(template)
-            //this.element = $(template);
-            //$(document.body).append(this.element);
-
-            const element = angular.element(document.getElementById('demo-slider'));
-            console.log('element: ', element);
-            //this.FlickityService.create(element[0], element[0].id)
+            this.compileDirective(template);
+            console.log('compiled: ', this.element[0].id);
 
 
+            this.FlickityService.create(this.element[0], 'js_demo').then((instance) => {
+                console.log('in test promise resolve: ', instance.id);
 
-            // Verify that two images were added
-            const actual = this.vm.instance.images.length;
-            const expected = 1;
-            expect(actual).toEqual(expected);
+                const actual = instance.id;
+                const expected = 'js_demo';
+
+                expect(actual).toEqual(expected);
+
+                console.log('before done');
+                done();
+            });
+
+
         });
 
     });
