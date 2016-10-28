288510706b56e5d7e5e672abf291518778fcb697
Benjamin Charity <ben@benjamincharity.com>
2016-10-28 14:31:09 -0400

    remove jQuery test resources

--- karma.conf.js
+++ karma.conf.js
@@ -18,8 +18,6 @@ module.exports = function(config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'node_modules/jquery/dist/jquery.min.js',
-            'node_modules/jasmine-jquery/lib/jasmine-jquery.js',
             'node_modules/angular/angular.js',
             'node_modules/angular-mocks/angular-mocks.js',
             'src/index.js',
@@ -58,7 +56,7 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['spec', 'coverage', 'coveralls'],
+        reporters: ['spec', 'coverage'/*, 'coveralls'*/],
 
 
         // web server port
