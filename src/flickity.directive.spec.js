8352ba201e44c6031208be177303934c91e577ad
Benjamin Charity <ben@benjamincharity.com>
2016-11-08 07:59:11 -0500

    :rotating_light: test instance destruction on directive destruction

--- src/flickity.directive.spec.js
+++ src/flickity.directive.spec.js
@@ -1,117 +1,87 @@
-/*
- *describe('FlickityDirective', () => {
- *    let $compile;
- *    let $rootScope;
- *
- *    // Include the module
- *    beforeEach(angular.mock.module('bc.Flickity'));
- *
- *    beforeEach(function() {
- *
- *        inject(function($compile, $rootScope) {
- *            this.$compile = $compile;
- *            this.$rootScope = $rootScope;
- *
- *            this.$scope = this.$rootScope.$new();
- *            //this.$scope.always = jasmine.createSpy('always');
- *            //this.$scope.done = jasmine.createSpy('done');
- *            //this.$scope.fail = jasmine.createSpy('fail');
- *            //this.$scope.progress = jasmine.createSpy('progress');
- *        });
- *
- *    });
- *
- *
- *    beforeEach(function() {
- *        this.compileDirective = function(template) {
- *            this.element = this.$compile(template)(this.$scope);
- *            this.vm = this.element.isolateScope().vm;
- *        };
- *    });
- *
- *
- *    afterEach(function() {
- *        this.element.remove();
- *    });
- *
- *    describe('create()', () => {
- *
- *        it(`should use $element when nothing is passed in`, function(done) {
- *            const template = angular.element(`
- *                <div
- *                    class="demo__slides"
- *                    bc-flickity="{{ flickityOptions }}"
- *                    bc-flickity-id="demoId1"
- *                >
- *                    <figure class="demo__slide" data-ng-repeat="slide in slides track by $index">
- *                        <img data-ng-src="{{ ::slide }}" alt="" />
- *                    </figure>
- *
- *                </div>
- *            `);
- *
- *            this.compileDirective(template);
- *
- *            // Wait for the images to load
- *            setTimeout(() => {
- *                // Verify that two images were added
- *                const actual = this.vm.instance.images.length;
- *                const expected = 1;
- *                expect(actual).toEqual(expected);
- *
- *                done();
- *            }, WAIT);
- *        });
- *
- *    });
- *
- *
- *
- *
- *
- *
- *    // Inject
- *    beforeEach(inject((_$compile_, _$rootScope_) => {
- *        $compile = _$compile_;
- *        $rootScope = _$rootScope_;
- *    }));
- *
- *
- *    describe('create()', () => {
- *        let $scope;
- *        let element;
- *        let $ctrl;
- *
- *        beforeEach(() => {
- *            $scope = $rootScope.$new();
- *            $scope.neededLength = 4;
- *            element = angular.element(`
- *                <div
- *                    class="demo__slides"
- *                    bc-flickity="{{ flickityOptions }}"
- *                    bc-flickity-id="demoId1"
- *                >
- *                    <figure class="demo__slide" data-ng-repeat="slide in slides track by $index">
- *                        <img data-ng-src="{{ ::slide }}" alt="" />
- *                    </figure>
- *
- *                </div>
- *            `);
- *            element = $compile(element)($scope);
- *            $scope.$apply();
- *            $ctrl = element.isolateScope().vm;
- *        });
- *
- *        it('should instantiate a Flickity instance', () => {
- *            //expect(typeof vm.bcNumberModel).toEqual('string');
- *        });
- *
- *        it('should create an ID if one was not passed in', () => {
- *        });
- *
- *    });
- *
- *});
- *
- *
- */
+describe('FlickityDirective', () => {
+    let $compile;
+    let $rootScope;
+
+    // Include the module
+    beforeEach(angular.mock.module('bc.Flickity'));
+
+    beforeEach(function() {
+
+        inject(function($compile, $rootScope, $timeout, FlickityService) {
+            this.$compile = $compile;
+            this.$rootScope = $rootScope;
+            this.$timeout = $timeout;
+            this.FlickityService = FlickityService;
+
+            this.$scope = this.$rootScope.$new();
+            this.$scope.slides = [
+                'http://cdn.benjamincharity.com/codepen/angular-flickity/slide1.jpg',
+                'http://cdn.benjamincharity.com/codepen/angular-flickity/slide2.jpg',
+                'http://cdn.benjamincharity.com/codepen/angular-flickity/slide3.jpg',
+                'http://cdn.benjamincharity.com/codepen/angular-flickity/slide4.jpg',
+            ];
+            this.$scope.extraSlide =
+                'http://cdn.benjamincharity.com/codepen/angular-flickity/slide5.jpg';
+        });
+
+    });
+
+
+    beforeEach(function() {
+        this.compileDirective = function(template) {
+            this.element = this.$compile(template)(this.$scope);
+            this.$scope.$digest();
+        };
+    });
+
+
+    afterEach(function() {
+        if (this.element) {
+            this.element.remove();
+        }
+    });
+
+    describe(`on $scope.$destroy()`, () => {
+
+        it(`should destroy the Flickity instance`, function(done) {
+            const template = angular.element(`
+                <div id="js_demo" bc-flickity>
+                    <figure data-ng-repeat="slide in slides track by $index">
+                        <img data-ng-src="{{ slide }}" alt="" />
+                    </figure>
+                </div>
+            `);
+            this.compileDirective(template);
+            const DELAY = 200;
+
+            this.$timeout(() => {
+                this.FlickityService.getAll().then((instances) => {
+                    const actual = instances.length;
+                    const expected = 1;
+
+                    expect(actual).toEqual(expected);
+
+                    this.$scope.$destroy();
+
+                    this.FlickityService.getAll().then((instances) => {
+                        const actual = instances.length;
+                        const expected = 0;
+
+                        expect(actual).toEqual(expected);
+
+                        done();
+                    });
+                });
+            });
+
+            this.$timeout.flush();
+
+            this.$timeout.verifyNoPendingTasks();
+        });
+
+    });
+
+
+});
+
+
